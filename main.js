4926d3f8a32c6458f17d8be3723fcf3418f2436a
Clara Gros <gros.clara91@gmail.com>
2023-12-02 16:39:16 +0100

    (fix) refactor code

--- main.js
+++ main.js
@@ -1,137 +1,135 @@
 // @ts-nocheck
 import "./style.css";
 
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1));
+};
+
 class Game {
-  constructor() {
-    this.targetNumber = null;
-    this.score = null;
+  constructor(min, max) {
+    this.min = min;
+    this.max = max;
+    this.targetNumber = getRandomNumber(this.min, this.max);
+    this.score = 0;
   }
 
   init() {
-    this.gameContainer = document.querySelector("#game");
+    this.gameContainer = document.querySelector("#game-container");
+    this.gameContainer.classList.remove("hidden");
 
-    // Attach events
-    this.form = document.querySelector("#form");
-    this.form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const value = new FormData(e.currentTarget).get("number");
-      this.onSubmit(value);
-    });
-    this.replayButton = document.querySelector("#replay");
-    this.replayButton.addEventListener("click", () => this.onReplay());
-    this.bar = document.querySelector("#bar");
-    this.result = document.querySelector("#answer");
+    this.form = document.querySelector("#guess-form");
+    this.form.addEventListener("submit", (e) => this.submitGuess(e));
 
-    this.showGame();
-    this.play();
+    this.restartButton = document.querySelector("#restart");
+    this.restartButton.addEventListener("click", () => this.restart());
+
+    this.attempts = document.querySelector("#attempts");
+    this.message = document.querySelector("#message");
+    this.scoreText = document.querySelector("#score");
+
+    this.start();
   }
 
-  showGame() {
-    this.startButton = document.querySelector("#start");
-    this.startButton.addEventListener("click", () => {
-      document.querySelector("#welcome").classList.add("hidden");
-      this.gameContainer.classList.remove("hidden");
-    });
-  }
+  submitGuess(event) {
+    event.preventDefault();
 
-  play() {
-    this.resetTargetNumber();
-    this.resetScore();
-  }
+    const form = event.currentTarget;
+    const guess = new FormData(form).get("guess");
 
-  endGame() {
-    this.result.innerHTML = `🟢 You found my secret number in ${this.score} attempts! It's ${this.targetNumber}! 🥳`;
+    if (!this.validateGuess(guess)) {
+      this.message.innerHTML = `❌ Invalid guess, you should enter a number between ${this.min} and ${this.max}!`;
+      this.message.classList.remove("invisible");
 
-    Array.from(this.form.firstElementChild.children).forEach((formElement) => {
-      formElement.setAttribute("disabled", "");
-    });
-
-    this.replayButton.classList.remove("hidden");
-  }
-
-  onSubmit(value) {
-    this.warningMsg = document.querySelector("#warning");
-    if (this.warningMsg.classList.contains("text-red-500"))
-      this.warningMsg.classList.replace("text-red-500", "text-gray-500");
-
-    if (!this.validateInput(value)) {
-      this.warningMsg.classList.replace("text-gray-500", "text-red-500");
-      console.log(this.warningMsg);
       this.resetForm();
       return;
     }
 
     this.updateScore();
-    this.updateBar(Number(value));
+    this.updateBar(Number(guess));
 
-    this.compareNumberWithTarget(Number(value));
+    this.compareNumberWithTarget(Number(guess));
   }
 
-  onReplay() {
+  start() {
+    this.targetNumber = getRandomNumber(this.min, this.max);
+    console.log("Target number: ", this.targetNumber);
+
+    this.resetScore();
+  }
+
+  restart() {
     this.resetForm();
-    this.resetBar();
+    this.resetAttempts();
 
-    this.result.classList.add("invisible");
-    this.replayButton.classList.add("hidden");
+    this.message.classList.add("invisible");
+    this.restartButton.classList.add("invisible");
 
-    this.play();
+    this.start();
   }
 
   compareNumberWithTarget(number) {
-    this.result.classList.remove("invisible");
-
     if (number !== this.targetNumber) {
-      this.result.innerHTML = `🔴 My number is ${
+      this.message.innerHTML = `🔴 My number is ${
         number > this.targetNumber ? "below" : "over"
       } ${number}.`;
+      this.message.classList.remove("invisible");
+
       this.form.reset();
       return;
     }
 
-    this.endGame();
+    this.message.innerHTML = `🟢 You found my secret number in ${this.score} attempts! It's ${this.targetNumber}! 🥳`;
+    this.message.classList.remove("invisible");
+
+    this.form.lastElementChild.setAttribute("disabled", "");
+
+    this.restartButton.classList.remove("invisible");
   }
 
-  validateInput(value) {
+  validateGuess(value) {
     return value && !Number.isNaN(value) && value >= 0 && value <= 500;
   }
 
-  resetTargetNumber() {
-    this.targetNumber = Math.floor(Math.random() * (500 + 1));
-    console.log("Target number: ", this.targetNumber);
-  }
-
-  resetBar() {
-    while (this.bar.hasChildNodes()) this.bar.removeChild(this.bar.firstChild);
+  resetAttempts() {
+    while (this.attempts.hasChildNodes())
+      this.attempts.removeChild(this.attempts.firstChild);
   }
 
   resetForm() {
     this.form.reset();
-    Array.from(this.form.firstElementChild.children).forEach((formElement) => {
-      formElement.removeAttribute("disabled", "");
-    });
+    this.form.lastElementChild.removeAttribute("disabled");
   }
 
   resetScore() {
     this.score = 0;
-    const scoreSpan = document.querySelector("#score");
-    scoreSpan.innerHTML = `Score: ${this.score}`;
+    this.scoreText.innerHTML = `Score: ${this.score}`;
   }
 
   updateScore() {
     this.score++;
-    const scoreSpan = document.querySelector("#score");
-    scoreSpan.innerHTML = `Score: ${this.score}`;
+    this.scoreText.innerHTML = `Score: ${this.score}`;
   }
 
   updateBar(number) {
-    const pin = document.createElement("p");
-    pin.append(`${number !== this.targetNumber ? "❌" : "🟢"}`);
-    pin.classList.add("absolute");
+    const attempt = document.createElement("p");
+    attempt.append(`${number !== this.targetNumber ? "❌" : "🟢"}`);
+    attempt.classList.add("absolute", "min-w-min");
 
-    this.bar.appendChild(pin);
-    pin.style.left = `${((number - pin.offsetWidth / 2) / 500) * 100}%`;
+    this.attempts.appendChild(attempt);
+    attempt.style.left = `${
+      ((number - attempt.offsetWidth / 2) / this.max) * 100
+    }%`;
   }
 }
 
-const game = new Game();
-game.init();
+const game = new Game(0, 500);
+
+const startGame = () => {
+  const startContainer = document.querySelector("#start-container");
+  startContainer.classList.add("hidden");
+
+  game.init();
+};
+
+const startButton = document.querySelector("#start");
+startButton.addEventListener("click", startGame);
